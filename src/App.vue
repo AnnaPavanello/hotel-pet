8291819a9907aa8b669b4836b2e5dd3dbdbde561
AnnaPavanello <annaclaramorettipavanello@gmail.com>
2026-09-22 11:51:35 -0300

    feito em aula com o sor

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import DefautLayout from './components/layout/DefautLayout.vue';
+import AppNavbar from './components/layout/AppNavbar.vue';
+import AppSlidebar from './components/layout/AppSlidebar.vue';
 </script>
 
 <template>
   <RouterView />
+  <DefautLayout/>
+  <AppNavbar/>
+  <AppSlidebar/>
 </template>
